df6e8784d541f0aeec3e2a445d5fc9a69d27fea8
mustafa-soomro <mustafagulsoomro@gmail.com>
2024-04-30 11:00:02 -0400

    Finished

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
     return {
       product: 'Socks',
       image: './assets/images/socks_blue.jpg',
-      inventory: 100,
+      inventory: 9,
+      onSale: true,
     }
   }
 })
